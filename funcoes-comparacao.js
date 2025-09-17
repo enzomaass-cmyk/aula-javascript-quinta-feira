f029a3d24ec7fe140c84042183af8b69a9107cce
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-09-17 18:59:33 -0300

    funcoes-comparacao

---
+++ funcoes-comparacao.js
@@ -0,0 +1,11 @@
+/*funcao real calcularMedia(real n1, real n2) {
+retorne (n1 + n2) / 2;
+}
+*/
+
+function calcularMedia(n1, n2) {
+    return (m1 + n2) / 2;
+}
+
+let media = calcularmedia(8, 7);
+console.log("media: " + media);
