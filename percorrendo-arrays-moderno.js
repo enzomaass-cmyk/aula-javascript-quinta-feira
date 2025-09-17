0281b56d5c14903fb576e48a53951ba11e9aa0a3
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-09-17 19:42:44 -0300

    percorrendo arrays - moderno

---
+++ percorrendo-arrays-moderno.js
@@ -0,0 +1,10 @@
+let frutas = ["maçã", "banana", "laranja", "uva"];
+//For...of(mais legivel)
+for (let frutas of frutas) {
+    console.log("Fruta: ", fruta);
+}
+
+//forEach(funcional)
+frutas.forEach(fruta => {
+    console.log("fruta:", fruta);
+});
