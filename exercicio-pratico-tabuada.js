c968a6c2a023a1392cbcd818fb11f9c0650d1cf5
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-09-17 18:44:57 -0300

    exercicio pratico tabuada

---
+++ exercicio-pratico-tabuada.js
@@ -0,0 +1,7 @@
+let numero = 7;
+console.log(`===Tabuada do ${numero}===`);
+
+for (let i = 1; i <= 10; i++) {
+    let resulatdo = numero * i;
+    console.log(`${numero} X ${i} = ${resulatdo}`);
+}
