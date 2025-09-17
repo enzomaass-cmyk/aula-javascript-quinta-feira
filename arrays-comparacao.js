628dc56bc508df31ff3fcc390ee5bc0759507939
enzomaass-cmyk <enzo_maass@estudante.sesisenai.org.br>
2025-09-17 19:28:47 -0300

    arrays comparacao

---
+++ arrays-comparacao.js
@@ -0,0 +1,13 @@
+/*inteiro numeros[5] = {10, 20, 30, 40, 50};
+
+para (inteiro i = 0; i < 5; i++) {
+escreva ("Posicao", i, ":" numeros[1])
+escreva ("\n") 
+}
+*/
+
+let numeros [10, 20, 30, 40, 50];
+
+for (let i  = 0; i < numeros.lenghth; i++) {
+    console.log(`Posicao ${i}: ${numeros[i]}`);
+}
